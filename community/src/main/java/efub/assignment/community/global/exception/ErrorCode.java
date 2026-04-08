de73a538de35219948cfc90d762af0b939104600
corjqnrl <coqnrl@ewha.ac.kr>
2026-04-08 14:08:42 +0900

    feat: error code 생성

---
+++ community/src/main/java/efub/assignment/community/global/exception/ErrorCode.java
@@ -0,0 +1,26 @@
+package efub.assignment.community.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    // Default
+    INTERNAL_SERVER_ERROR(500, "예상치 못한 서버 에러가 발생하였습니다."),
+    ERROR(400, "요청 처리에 실패했습니다."),
+
+    // member
+    ACCOUNT_NOT_FOUND(404, "존재하지 않는 계정입니다."),
+
+    // post
+    POST_NOT_FOUND(404, "존재하지 않는 게시물입니다."),
+    POST_ACCOUNT_MISMATCH(403, "게시글 생성자가 아닙니다."),
+
+    // board
+    BOARD_NOT_FOUND(404, "존재하지 않는 게시판입니다."),
+    BOARD_ACCOUNT_MISMATCH(403, "게시판 생성자가 아닙니다.");
+
+    private final int status;
+    private final String message;
+}
