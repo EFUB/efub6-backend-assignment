8d9b3f07ea26ef0584cd4ff3ef3868c23a92bd64
corjqnrl <coqnrl@ewha.ac.kr>
2026-04-09 17:12:55 +0900

    feat: board repository 생성

--- community/src/main/java/efub/assignment/community/board/repository/BoardRepository.java
+++ community/src/main/java/efub/assignment/community/board/repository/BoardRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    // 게시판 존재 확인
-    Optional<Board> findByBoardId(Long boardId);
 }
 
