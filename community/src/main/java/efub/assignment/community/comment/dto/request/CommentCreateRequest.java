4e54c9c98959fda7999c7983b731333abd57ef96
corjqnrl <coqnrl@ewha.ac.kr>
2026-04-30 16:30:49 +0900

    feat: comment 생성/조회/수정 request, response dto 생성

---
+++ community/src/main/java/efub/assignment/community/comment/dto/request/CommentCreateRequest.java
@@ -0,0 +1,26 @@
+package efub.assignment.community.comment.dto.request;
+
+import efub.assignment.community.comment.domain.Comment;
+import efub.assignment.community.member.domain.Member;
+import efub.assignment.community.post.domain.Post;
+import lombok.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class CommentCreateRequest {
+
+    // comment 생성 시 작성자ID, content 전달받기
+    private Long memberId;
+    private String content;
+
+    // member와 post에서 가져와 사용
+    public Comment toEntity(Member member, Post post) {
+        return Comment.builder()
+                .content(content)
+                .writer(member)
+                .post(post)
+                .build();
+    }
+}
