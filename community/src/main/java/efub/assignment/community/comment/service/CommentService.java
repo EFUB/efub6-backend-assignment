d73fe941ed4e4c5bec04110afb199d9cda6bb624
corjqnrl <coqnrl@ewha.ac.kr>
2026-04-30 16:34:31 +0900

    feat: comment service 생성

---
+++ community/src/main/java/efub/assignment/community/comment/service/CommentService.java
@@ -0,0 +1,77 @@
+package efub.assignment.community.comment.service;
+
+import efub.assignment.community.comment.domain.Comment;
+import efub.assignment.community.comment.dto.request.CommentCreateRequest;
+import efub.assignment.community.comment.dto.request.CommentUpdateRequest;
+import efub.assignment.community.comment.repository.CommentRepository;
+import efub.assignment.community.global.exception.CustomException;
+import efub.assignment.community.global.exception.ErrorCode;
+import efub.assignment.community.member.domain.Member;
+import efub.assignment.community.member.dto.response.MemberCommentResponse;
+import efub.assignment.community.member.service.MemberService;
+import efub.assignment.community.post.domain.Post;
+import efub.assignment.community.post.dto.response.PostCommentResponse;
+import efub.assignment.community.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final MemberService memberService;
+    private final PostService postService;
+    private final CommentRepository commentRepository;
+    private ErrorCode ErrorCode;
+
+    // 댓글 생성
+    @Transactional
+    public Long createComment(Long postId, CommentCreateRequest request) {
+        Long memberId = request.getMemberId();
+        Member writer = memberService.findByMemberId(memberId);
+        Post post = postService.findByPostId(postId);
+
+        Comment newComment = request.toEntity(writer, post);
+        commentRepository.save(newComment);
+
+        return newComment.getId();
+    }
+
+    // 특정 게시물 댓글 조회
+    @Transactional
+    public PostCommentResponse getPostCommentList(Long postId) {
+        List<Comment> commentList = commentRepository.findAllByPostIdOrderByCreatedAtDesc(postId);
+        return PostCommentResponse.of(postId, commentList);
+    }
+
+    // 특정 사용자 댓글 조회
+    @Transactional
+    public MemberCommentResponse getMemberCommentList(Long memberId) {
+        List<Comment> commentList = commentRepository.findAllByWriterMemberIdOrderByCreatedAtDesc(memberId);
+        return MemberCommentResponse.of(memberId, commentList);
+    }
+
+    // 댓글 수정
+    @Transactional
+    public void updateCommentContent(Long commentId, CommentUpdateRequest request, Long memberId) {
+        Comment comment = findByCommentId(commentId);
+        Member member = memberService.findByMemberId(memberId);
+
+        authorizeCommentWriter(comment, member);
+        comment.changeContent(request.content());
+    }
+
+    private void authorizeCommentWriter(Comment comment, Member member) {
+        if(!comment.getWriter().equals(member)) {
+            throw new CustomException(ErrorCode.COMMENT_ACCOUNT_MISMATCH);
+        }
+    }
+
+    private Comment findByCommentId(Long commentId) {
+        return commentRepository.findById(commentId)
+                .orElseThrow(() -> new CustomException(ErrorCode.COMMENT_NOT_FOUND));
+    }
+}
