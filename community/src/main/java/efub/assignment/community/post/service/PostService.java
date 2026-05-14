d37695cd6f3f6dbe632727fd3d3bcdc1964e34e9
corjqnrl <coqnrl@ewha.ac.kr>
2026-05-14 20:10:50 +0900

    feat: 게시물 좋아요 생성/삭제

--- community/src/main/java/efub/assignment/community/post/service/PostService.java
+++ community/src/main/java/efub/assignment/community/post/service/PostService.java
@@ -7,11 +7,13 @@ import efub.assignment.community.global.exception.ErrorCode;
 import efub.assignment.community.member.domain.Member;
 import efub.assignment.community.member.service.MemberService;
 import efub.assignment.community.post.domain.Post;
+import efub.assignment.community.post.domain.PostLike;
 import efub.assignment.community.post.dto.request.PostCreateRequest;
 import efub.assignment.community.post.dto.request.PostUpdateRequest;
 import efub.assignment.community.post.dto.response.PostListResponse;
 import efub.assignment.community.post.dto.response.PostResponse;
 import efub.assignment.community.post.dto.summary.PostSummary;
+import efub.assignment.community.post.repository.PostLikeRepository;
 import efub.assignment.community.post.repository.PostRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +29,7 @@ public class PostService {
     private final MemberService memberService;
     private final BoardService boardService;
     private final PostRepository postRepository;
+    private final PostLikeRepository postLikeRepository;
 
     // 게시글 생성
     @Transactional
@@ -78,6 +81,30 @@ public class PostService {
         // 게시굴 작성자 = 계정 사용자
         authorizePostWriter(post, member);
         postRepository.delete(post);
+    }
+
+    // 게시물 좋아요 생성
+    public void createPostLike(Long postId, Long memberId) {
+        Post post = findByPostId(postId);
+        Member member = memberService.findByMemberId(memberId);
+        if (postLikeRepository.existsByPostAndMember(post, member)) {
+            throw new CustomException(ErrorCode.LIKE_ALREADY_EXISTS);
+        }
+
+        PostLike like = PostLike.builder()
+                .post(post)
+                .member(member)
+                .build();
+        postLikeRepository.save(like);
+    }
+
+    // 게시물 좋아요 삭제
+    public void deletePostLike(Long postId, Long memberId) {
+        Post post = findByPostId(postId);
+        Member member = memberService.findByMemberId(memberId);
+        PostLike like = postLikeRepository.findByPostAndMember(post, member)
+                .orElseThrow(() -> new CustomException(ErrorCode.LIKE_NOT_FOUND));
+        postLikeRepository.delete(like);
     }
 
     private void authorizePostWriter(Post post, Member member) {
