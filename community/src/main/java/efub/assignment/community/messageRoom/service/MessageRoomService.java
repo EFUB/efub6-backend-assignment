26690a603a3b98828293f230ea51b87723ca79ea
corjqnrl <coqnrl@ewha.ac.kr>
2026-05-27 11:47:27 +0900

    fix: 쪽지방이 존재하지 않을 때 빈 배열 대신 404 errorcode 발생하도록 수정

--- community/src/main/java/efub/assignment/community/messageRoom/service/MessageRoomService.java
+++ community/src/main/java/efub/assignment/community/messageRoom/service/MessageRoomService.java
@@ -62,7 +62,7 @@ public class MessageRoomService {
         Optional<MessageRoom> messageRoom = messageRoomRepository.findBySenderAndReceiverAndPost(sender, receiver, post);
 
         return messageRoom.map(GetMessageRoomResponse::from)
-                .orElse(GetMessageRoomResponse.empty());
+                .orElseThrow(() -> new CustomException(ErrorCode.MESSAGEROOM_NOT_FOUND));
     }
 
     // 쪽지방 삭제
