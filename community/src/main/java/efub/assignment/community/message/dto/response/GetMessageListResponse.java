fe3051b9beb57faa1822323343442e3fc108fd01
corjqnrl <coqnrl@ewha.ac.kr>
2026-05-27 12:02:07 +0900

    fix: 쪽지방 조회 시 receiverId로 상대방 id를 반환하도록 수정

--- community/src/main/java/efub/assignment/community/message/dto/response/GetMessageListResponse.java
+++ community/src/main/java/efub/assignment/community/message/dto/response/GetMessageListResponse.java
@@ -1,5 +1,6 @@
 package efub.assignment.community.message.dto.response;
 
+import com.fasterxml.jackson.databind.deser.impl.CreatorCandidate;
 import efub.assignment.community.message.domain.Message;
 import efub.assignment.community.messageRoom.domain.MessageRoom;
 import lombok.Builder;
@@ -19,7 +20,8 @@ public class GetMessageListResponse {
     public static GetMessageListResponse from(MessageRoom messageRoom, List<Message> messageList, Long requesterId) {
         return GetMessageListResponse.builder()
                 .messageRoomId(messageRoom.getId())
-                .receiverId(messageRoom.getReceiver().getMemberId())
+                .receiverId(messageRoom.getReceiver().getMemberId().equals(requesterId)
+                    ? messageRoom.getSender().getMemberId() : messageRoom.getReceiver().getMemberId())
                 .messages(messageList.stream()
                         .map(message -> MessageResponse.from(message, requesterId))
                         .collect(Collectors.toList()))
